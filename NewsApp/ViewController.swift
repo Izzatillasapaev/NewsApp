f10968ef3c270ae4ed99e0b9adda47b6362171ab
Izzatilla <Izzatilla.sapayev@gmail.com>
2020-03-29 10:04:29 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Izzatilla on 29.03.2020.
+//  Copyright © 2020 Izzatilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
